2d84ea1079af47269e2a455cd7287b1f82988530
Parth V. Parikh <parth.v.parikh@keysight.com>
2025-08-28 18:15:37 -0700

    fix(keymap): fix some keymaps

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,4 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>nt", vim.cmd.tabnew)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>nt", vim.cmd.tabnew)
 vim.keymap.set("n", "<leader>z", vim.cmd.Yazi)
