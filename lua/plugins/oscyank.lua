a87745401c09281e7d829f427ff380479550d18b
Parth V. Parikh <parth.v.parikh@keysight.com>
2026-08-07 16:26:08 -0700

    Wire nvim-osc52 into g:clipboard for automatic remote clipboard sync
    Manual <leader>c copy worked, but plain unnamedplus yanks silently
    failed over ssh since there's no xclip/wl-copy on the remote host.

--- lua/plugins/oscyank.lua
+++ lua/plugins/oscyank.lua
@@ -5,5 +5,18 @@ return {
 		vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, {expr = true})
 		vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true})
 		vim.keymap.set('v', '<leader>c', require('osc52').copy_visual)
+
+		-- Force unnamedplus yanks to go out via OSC52 (no system clipboard tool over ssh)
+		local function copy(lines, _)
+			require("osc52").copy(table.concat(lines, "\n"))
+		end
+		local function paste()
+			return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
+		end
+		vim.g.clipboard = {
+			name = "osc52",
+			copy = { ["+"] = copy, ["*"] = copy },
+			paste = { ["+"] = paste, ["*"] = paste },
+		}
 	end
 }
