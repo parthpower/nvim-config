152b71a56fc8527252db838a44d0dfbb438dbbdd
Parth V. Parikh <parth.v.parikh@keysight.com>
2026-08-20 20:39:53 -0700

    fix(clipboard): for reasons it was broken

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,14 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.expandtab = true
   end,
 })
+
+local osc52 = require("vim.ui.clipboard.osc52")
+local function paste()
+  return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
+end
+
+vim.g.clipboard = {
+  name = "osc52",
+  copy = { ["+"] = osc52.copy("+"), ["*"] = osc52.copy("*") },
+  paste = { ["+"] = paste, ["*"] = paste },
+}
